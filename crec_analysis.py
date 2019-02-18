e0f45657609b5cc64476c97b17a5a01c106d8dd5
Zoltan P. Majdik <zoltan@zoltanmajdik.com>
2019-02-18 15:28:33 -0600

    final edits to figures

--- crec_analysis.py
+++ crec_analysis.py
@@ -32,8 +32,7 @@ def df_main():
     """Pull full database into dataframe"""
     crec_df = pd.read_sql("Select * from crec", CON, index_col='UTC')
     crec_df['html_data'] = crec_df['html_data'].str.replace('\n', ' ')
-    crec_df = crec_df.set_index(pd.DatetimeIndex(crec_df.index)).ix[:'2016-12-31']
-    #control date range of df
+    crec_df = crec_df.set_index(pd.DatetimeIndex(crec_df.index)).ix[:'2016-12-31'] #control date range of df
     return crec_df
 
 def df_reduce():
@@ -56,7 +55,7 @@ def df_map():
     for chunk in pd.read_sql("Select * from crec", CON, index_col='UTC',
                              chunksize=1000):
         chunk['html_data'] = chunk['html_data'].str.replace('\n', ' ')
-        chunk = chunk.set_index(pd.DatetimeIndex(chunk.index)).ix[:'2017-12-31']
+        chunk = chunk.set_index(pd.DatetimeIndex(chunk.index)).ix[:'2016-12-31']
         chunk = chunk[chunk.html_data.str.contains(MAIN_KEYWORD)]
 
         query1_list.append(pd.DataFrame(index=chunk.index,
@@ -111,19 +110,56 @@ def plot_frame():
     rounded_ratiolist_ratio = [round(float(elem),3) for elem in
             ratio_list_ratio]
 
-    fig1 = plt.figure(figsize=(13,15))
+    fig = plt.figure(figsize=(13,15))
 
-    ax1=plt.subplot(211)
+    ax1 = plt.subplot(211)
     ax1.set_xlabel('Year', fontweight='bold')
     ax1.set_ylabel('Frequency', fontweight='bold')
-    ax1.set_title('Agentic Frames', fontweight='bold', pad=15)
-    plot1 = plt.bar(np.unique(plot1_data.index.year),
-            plot1_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.8, color="#FF5500", width=.4, align='edge')
+    ax1.set_title('Agentic Frames in the Congressional Record', fontweight='bold', pad=15)
     plot2 = plt.bar(np.unique(plot2_data.index.year),
-            plot2_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.5, color="#6495ED")
-    plt.legend((plot1, plot2), (QUERY1_DESC, QUERY2_DESC), loc=2, fontsize=17)
+                    plot2_data['html_data'].groupby(lambda x: x.year).sum(),
+                    alpha=0.5, color="#6495ED")
+    plot1 = plt.bar(np.unique(plot1_data.index.year),
+                    plot1_data['html_data'].groupby(lambda x: x.year).sum(),
+                    alpha=0.7, color="#FF5500", width=.4, align='edge')
+    plt.legend((plot1, plot2, (plot1, plot2)), (QUERY1_DESC, QUERY2_DESC, 'Corpus-wide active/passive = {}'.format(
+        round(sum(rounded_ratiolist_frame) / len(rounded_ratiolist_frame), 3))), loc=2, fontsize=17)
+
+    ax1.annotate('', xy=(1995, -79), xytext=(2000, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('R. control', xy=(1997.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2001, -79), xytext=(2002, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('D. control*', xy=(2001.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2003, -79), xytext=(2006, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('R. control', xy=(2004.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2007, -79), xytext=(2010, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('D. control', xy=(2008.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2011, -79), xytext=(2014, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('Split control', xy=(2012.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2015, -79), xytext=(2016, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('R. control', xy=(2015.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(1997.5, 140), xytext=(1996.5, 190), arrowprops=dict(arrowstyle='->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('Kyoto protocol adopted', xy=(1995, 210), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2005, 360), xytext=(2004, 410), arrowprops=dict(arrowstyle='->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('Kyoto protocol goes into effect', xy=(2002.5, 430), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2006, 140), xytext=(2006, 190), arrowprops=dict(arrowstyle='->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('H.R. \n 5642', xy=(2006, 210), ha='center', annotation_clip=False)
 
     def autolabel_frames(rects):
         """
@@ -136,16 +172,16 @@ def plot_frame():
                     ha='center', va='bottom')
     autolabel_frames(plot2)
 
-    ax2=plt.subplot(212)
+    ax2 = plt.subplot(212)
     ax2.set_xlabel('Year', fontweight='bold')
     ax2.set_ylabel('Frequency', fontweight='bold')
-    ax2.set_title('Pentadic Frames/Ratios', fontweight='bold', pad=15)
-    plot3 = plt.bar(np.unique(plot3_data.index.year),
-            plot3_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.8, color="#FF5500", width=.4, align='edge')
+    ax2.set_title('Pentadic Ratios in the Congressional Record', fontweight='bold', pad=15)
     plot4 = plt.bar(np.unique(plot4_data.index.year),
-            plot4_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.5, color="#6495ED")
+                    plot4_data['html_data'].groupby(lambda x: x.year).sum(),
+                    alpha=0.5, color="#6495ED")
+    plot3 = plt.bar(np.unique(plot3_data.index.year),
+                    plot3_data['html_data'].groupby(lambda x: x.year).sum(),
+                    alpha=0.7, color="#FF5500", width=.4, align='edge')
     plt.legend((plot3, plot4), (QUERY3_DESC, QUERY4_DESC), loc=2, fontsize=17)
 
     def autolabel_ratios(rects):
@@ -159,74 +195,59 @@ def plot_frame():
                     ha='center', va='bottom')
     autolabel_ratios(plot4)
 
-    fig1.savefig('fig.png')
+    fig.savefig('fig.png')
 
 
-
-    fig2 = plt.figure(figsize=(13,15))
-
-    ax1=plt.subplot(211)
-    ax1.set_xlabel('Year', fontweight='bold')
-    ax1.set_ylabel('Frequency', fontweight='bold')
-    ax1.set_title('Agentic Frames', fontweight='bold', pad=15)
-    plot1 = plt.bar(np.unique(plot1_data.index.year),
-            plot1_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.8, color="#FF5500", width=.4, align='edge')
-    plot2 = plt.bar(np.unique(plot2_data.index.year),
-            plot2_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.5, color="#6495ED", width=.4)
-    plt.legend((plot1, plot2), (QUERY1_DESC, QUERY2_DESC), loc=2, fontsize=17)
-
-    def autolabel_frames(rects):
-        """
-        Attach a text label above each bar displaying its height
-        """
-        for rect,item in zip(rects, rounded_ratiolist_frame):
-            height = rect.get_height()
-            ax1.text(rect.get_x() + rect.get_width()/2., 1.50+height,
-                    item,
-                    ha='center', va='bottom')
-    autolabel_frames(plot2)
-
-    ax2=plt.subplot(212)
-    ax2.set_xlabel('Year', fontweight='bold')
-    ax2.set_ylabel('Frequency', fontweight='bold')
-    ax2.set_title('Pentadic Frames/Ratios', fontweight='bold', pad=15)
-    plot3 = plt.bar(np.unique(plot3_data.index.year),
-            plot3_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.8, color="#FF5500", width=.4, align='edge')
-    plot4 = plt.bar(np.unique(plot4_data.index.year),
-            plot4_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.5, color="#6495ED", width=.4)
-    plt.legend((plot3, plot4), (QUERY3_DESC, QUERY4_DESC), loc=2, fontsize=17)
-
-    def autolabel_ratios(rects):
-        """
-        Attach a text label above each bar displaying its height
-        """
-        for rect,item in zip(rects, rounded_ratiolist_ratio):
-            height = rect.get_height()
-            ax2.text(rect.get_x() + rect.get_width()/2., 1.50+height,
-                    item,
-                    ha='center', va='bottom')
-    autolabel_ratios(plot4)
-
-    fig2.savefig('fig2.png')
-
-
-    fig3 = plt.figure(figsize=(13,15))
+    fig_bw = plt.figure(figsize=(13,15))
 
     ax1=plt.subplot(211)
     ax1.set_xlabel('Year', fontweight='bold')
     ax1.set_ylabel('Frequency', fontweight='bold')
-    ax1.set_title('Agentic Frames', fontweight='bold', pad=15)
+    ax1.set_title('Agentic Frames in the Congressional Record', fontweight='bold', pad=15)
     plot1 = plt.bar(np.unique(plot1_data.index.year),
             plot1_data['html_data'].groupby(lambda x:x.year).sum(),
             alpha=0.8, color="#000000", width=.4, align='edge')
     plot2 = plt.bar(np.unique(plot2_data.index.year),
             plot2_data['html_data'].groupby(lambda x:x.year).sum(),
-            alpha=0.5, color="#a8a8a8", width=.4)
-    plt.legend((plot1, plot2), (QUERY1_DESC, QUERY2_DESC), loc=2, fontsize=17)
+            alpha=0.5, color="#a8a8a8")
+    plt.legend((plot1, plot2, (plot1, plot2)), (QUERY1_DESC, QUERY2_DESC, 'Corpus-wide active/passive = {}'.format(
+        round(sum(rounded_ratiolist_frame) / len(rounded_ratiolist_frame), 3))), loc=2, fontsize=17)
+
+    ax1.annotate('', xy=(1995, -79), xytext=(2000, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('R. control', xy=(1997.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2001, -79), xytext=(2002, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('D. control*', xy=(2001.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2003, -79), xytext=(2006, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('R. control', xy=(2004.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2007, -79), xytext=(2010, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('D. control', xy=(2008.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2011, -79), xytext=(2014, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('Split control', xy=(2012.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2015, -79), xytext=(2016, -79), arrowprops=dict(arrowstyle='<->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('R. control', xy=(2015.5, -109), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(1997.5, 140), xytext=(1996.5, 190), arrowprops=dict(arrowstyle='->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('Kyoto protocol adopted', xy=(1995, 210), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2005, 360), xytext=(2004, 410), arrowprops=dict(arrowstyle='->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('Kyoto protocol goes into effect', xy=(2002.5, 430), ha='center', annotation_clip=False)
+
+    ax1.annotate('', xy=(2006, 140), xytext=(2006, 190), arrowprops=dict(arrowstyle='->', facecolor='black'),
+                 annotation_clip=False)
+    ax1.annotate('H.R. \n 5642', xy=(2006, 210), ha='center', annotation_clip=False)
 
     def autolabel_frames(rects):
         """
@@ -237,12 +258,12 @@ def plot_frame():
             ax1.text(rect.get_x() + rect.get_width()/2., 1.50+height,
                     item,
                     ha='center', va='bottom')
-    autolabel_frames(plot2)
+    autolabel_frames(plot1)
 
     ax2=plt.subplot(212)
     ax2.set_xlabel('Year', fontweight='bold')
     ax2.set_ylabel('Frequency', fontweight='bold')
-    ax2.set_title('Pentadic Frames/Ratios', fontweight='bold', pad=15)
+    ax2.set_title('Pentadic Ratios in the Congressional Record', fontweight='bold', pad=15)
     plot3 = plt.bar(np.unique(plot3_data.index.year),
             plot3_data['html_data'].groupby(lambda x:x.year).sum(),
             alpha=0.8, color="#000000", width=.4, align='edge')
@@ -260,6 +281,6 @@ def plot_frame():
             ax2.text(rect.get_x() + rect.get_width()/2., 1.50+height,
                     item,
                     ha='center', va='bottom')
-    autolabel_ratios(plot4)
+    autolabel_ratios(plot3)
 
-    fig3.savefig('fig3.png')
+    fig_bw.savefig('fig_bw.png')
